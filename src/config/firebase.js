6b7032bf00823b5e98f3a022273517b6942c21e2
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-11-23 23:49:27 +0500

    Auth Stepup and Firebase Integrated

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB_HllNsnuKF8R6UbKZiUcKsAM4Rp3cdPY",
+  authDomain: "auth-dashboard-starter-kit.firebaseapp.com",
+  projectId: "auth-dashboard-starter-kit",
+  storageBucket: "auth-dashboard-starter-kit.firebasestorage.app",
+  messagingSenderId: "916381672200",
+  appId: "1:916381672200:web:4196a507d697eccdacc2bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
