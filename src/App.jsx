164aff2eb138a64278b74302cc053c3787300883
ZUBAIN NADEEM <zubainali23@gmail.com>
2025-11-24 00:06:53 +0500

    Project Started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Login from "./components/auth/Login"
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Login />
     </div>
   )
 }
---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='bg-[#588157] w-full h-screen flex justify-center items-center'>
+            {/* Login Page */}
+            <section className='w-[45%] min-h-[300px] rounded-2xl shadow-sm shadow-[#a3b18a] p-5'>
+                
+            </section>  
+            {/* Login Page */}
+        </div>
+    )
+}
+
+export default Login
